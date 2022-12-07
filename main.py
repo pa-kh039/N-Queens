a5f24bf44df560a74177cfaa2083dbcb37b131e5
PARTH KHANDENATH <parthkhandenath123@gmail.com>
2022-12-07 20:50:59 +0530

    comment

--- main.py
+++ main.py
@@ -45,6 +45,7 @@ n = int(input())
 l=[[0 for i in range(n)] for j in range(n)]
 nQueen(n,0,l)
 # ---------------------------------------------------------------------------------------------------
+# Demonstartes Backtracking....
 
 
 
